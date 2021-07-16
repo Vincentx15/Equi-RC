e2228c8946b1f0f10639f593dc5b0c074744cacb
Vincentx15 <vincent.mallet96@gmail.com>
2021-07-16 12:19:19 +0200

    Small modifs in the example model implementation

--- pytorch_example.py
+++ pytorch_example.py
@@ -1,8 +1,71 @@
 import torch
 import torch.nn as nn
 
-from pytorch_rclayers import RegToIrrepConv, IrrepToIrrepConv, IrrepActivationLayer, \
-    IrrepConcatLayer, IrrepBatchNorm, ToKmerLayer
+from pytorch_rclayers import RegToRegConv, RegToIrrepConv, IrrepToIrrepConv, IrrepActivationLayer, \
+    IrrepConcatLayer, IrrepBatchNorm, RegBatchNorm, RegConcatLayer, ToKmerLayer
+
+
+class CustomRCPS(nn.Module):
+
+    def __init__(self,
+                 filters=(16, 16, 16),
+                 kernel_sizes=(15, 14, 14),
+                 pool_size=40,
+                 pool_strides=20,
+                 out_size=1,
+                 placeholder_bn=False,
+                 kmers=1):
+        """
+        This is an example of use of the equivariant layers :
+
+        The network takes as inputs windows of 1000 base pairs one hot encoded and outputs a binary prediction
+        The architecture follows the paper of Avanti Shrikumar : Reverse Complement Parameter Sharing
+        We reimplement everything with equivariant layers and add the possibility to start the encoding with
+        a K-Mer encoding layer.
+        """
+        super(CustomRCPS, self).__init__()
+
+        self.kmers = int(kmers)
+        self.to_kmer = ToKmerLayer(k=self.kmers)
+        reg_in = self.to_kmer.features // 2
+        filters = [reg_in] + list(filters)
+
+        # Now add the intermediate layer : sequence of conv, BN, activation
+        self.reg_layers = nn.ModuleList()
+        self.bn_layers = nn.ModuleList()
+        self.activation_layers = nn.ModuleList()
+        for i in range(len(filters) - 1):
+            prev_reg = filters[i]
+            next_reg = filters[i + 1]
+            self.reg_layers.append(RegToRegConv(
+                reg_in=prev_reg,
+                reg_out=next_reg,
+                kernel_size=kernel_sizes[i],
+            ))
+            self.bn_layers.append(RegBatchNorm(reg_dim=next_reg, placeholder=placeholder_bn))
+            # Don't add activation if it's the last layer
+            placeholder = (i == len(filters) - 1)
+            self.activation_layers.append(nn.ReLU())
+
+        self.concat = RegConcatLayer(reg=filters[-1])
+        self.pool = nn.MaxPool1d(kernel_size=pool_size, stride=pool_strides)
+        self.flattener = nn.Flatten()
+        self.dense = nn.Linear(in_features=752, out_features=out_size)
+
+    def forward(self, inputs):
+        x = self.to_kmer(inputs)
+        for reg_layer, bn_layer, activation_layer in zip(self.reg_layers, self.bn_layers, self.activation_layers):
+            x = reg_layer(x)
+            x = bn_layer(x)
+            x = activation_layer(x)
+
+        # Average two strands predictions, pool and go through Dense
+        x = self.concat(x)
+        x = self.pool(x)
+        x = self.flattener(x)
+        x = self.dense(x)
+        outputs = torch.sigmoid(x)
+        return outputs
 
 
 class EquiNetBinary(nn.Module):
@@ -16,7 +79,9 @@ class EquiNetBinary(nn.Module):
                  placeholder_bn=False,
                  kmers=1):
         """
-        First map the regular representation to irrep setting
+        This network takes as inputs windows of 1000 base pairs one hot encoded and outputs a binary prediction
+
+        First maps the regular representation to irrep setting
         Then goes from one setting to another.
         """
         super(EquiNetBinary, self).__init__()
@@ -37,9 +102,9 @@ class EquiNetBinary(nn.Module):
         self.first_act = IrrepActivationLayer(a=first_a, b=first_b)
 
         # Now add the intermediate layers : sequence of conv, BN, activation
-        self.irrep_layers = []
-        self.bn_layers = []
-        self.activation_layers = []
+        self.irrep_layers = nn.ModuleList()
+        self.bn_layers = nn.ModuleList()
+        self.activation_layers = nn.ModuleList()
         for i in range(1, len(filters)):
             prev_a, prev_b = filters[i - 1]
             next_a, next_b = filters[i]
